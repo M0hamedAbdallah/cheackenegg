e2f8dfd5751651e21ec6d7ef4335fd36ddfdfd19
mostafaturk <121161662+mostafaturk@users.noreply.github.com>
2022-12-27 01:11:00 +0200

    Update Cheaken.java

--- checken/Cheaken.java
+++ checken/Cheaken.java
@@ -242,78 +242,7 @@ public class Cheaken extends AnimListener implements GLEventListener, MouseListe
 
         gl.glDisable(GL.GL_BLEND);
     }
-
-    public void squreOfHome(GL gl, int index) {
-        gl.glEnable(GL.GL_BLEND);	// Turn Blending On
-        gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[index]);
-
-        gl.glPushMatrix();
-
-        gl.glBegin(GL.GL_QUADS);
-
-        // Front Face
-        gl.glTexCoord2f(0.0f, 0.0f);
-        gl.glVertex3f(0f, 0f, -1.0f);
-
-        gl.glTexCoord2f(1.0f, 0.0f);
-        gl.glVertex3f(700, 0f, -1.0f);
-
-        gl.glTexCoord2f(1.0f, 1.0f);
-        gl.glVertex3f(700f, 700f, -1.0f);
-
-        gl.glTexCoord2f(0.0f, 1.0f);
-        gl.glVertex3f(0f, 700f, -1.0f);
-
-        gl.glEnd();
-        gl.glPopMatrix();
-
-        gl.glDisable(GL.GL_BLEND);
-    }
-
-    @Override
-    public void init(GLAutoDrawable gld) {
-        GL gl = gld.getGL();
-        gl.glViewport(0, 0, 700, 700);
-//        gl.glClearColor(0.0f, 1.0f, 0.0f, 1.0f);    //This Will Clear The Background Color To Black
-        gl.glMatrixMode(GL.GL_PROJECTION);
-//        gl.glOrtho(-450, 450, -250, 250, -1.0, 1.0);
-        gl.glOrtho(0, 700, 0, 700, -1.0, 1.0);
-        
-        try {
-            music = new FileInputStream(new File("Music//chicken dance song.wav"));
-            audios = new AudioStream(music);
-        } catch (IOException ex) {
-            System.err.println(ex.getMessage());
-        }
-        AudioPlayer.player.start(audios);
-
-        gl.glEnable(GL.GL_TEXTURE_2D);  // Enable Texture Mapping
-        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
-
-        //number of textures, array to hold the indeces
-        gl.glGenTextures(textureName.length, textureIndex, 0);
-
-        for (int i = 0; i < textureName.length; i++) {
-            try {
-                texture = TextureReader.readTexture(assetsFolderName + "//" + textureName[i], true);
-                gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[i]);
-
-//          mipmapsFromPNG(gl, new GLU(), texture[i]);
-                new GLU().gluBuild2DMipmaps(
-                        GL.GL_TEXTURE_2D,
-                        GL.GL_RGBA, // Internal Texel Format,
-                        texture.getWidth(), texture.getHeight(),
-                        GL.GL_RGBA, // External format from image,
-                        GL.GL_UNSIGNED_BYTE,
-                        texture.getPixels() // Imagedata
-                );
-            } catch (IOException e) {
-                System.out.println(e);
-                e.printStackTrace();
-            }
-        }
-    }
-     public void squreOFegg2(GL gl, int index) {
+       public void squreOFegg2(GL gl, int index) {
         gl.glEnable(GL.GL_BLEND);	// Turn Blending On
         gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[index]);
 
@@ -350,6 +279,33 @@ public class Cheaken extends AnimListener implements GLEventListener, MouseListe
 
         gl.glTexCoord2f(0.0f, 1.0f);
         gl.glVertex3f(0f, 50, -1.0f);
+
+        gl.glEnd();
+        gl.glPopMatrix();
+
+        gl.glDisable(GL.GL_BLEND);
+    }
+
+    public void squreOfHome(GL gl, int index) {
+        gl.glEnable(GL.GL_BLEND);	// Turn Blending On
+        gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[index]);
+
+        gl.glPushMatrix();
+
+        gl.glBegin(GL.GL_QUADS);
+
+        // Front Face
+        gl.glTexCoord2f(0.0f, 0.0f);
+        gl.glVertex3f(0f, 0f, -1.0f);
+
+        gl.glTexCoord2f(1.0f, 0.0f);
+        gl.glVertex3f(700, 0f, -1.0f);
+
+        gl.glTexCoord2f(1.0f, 1.0f);
+        gl.glVertex3f(700f, 700f, -1.0f);
+
+        gl.glTexCoord2f(0.0f, 1.0f);
+        gl.glVertex3f(0f, 700f, -1.0f);
 
         gl.glEnd();
         gl.glPopMatrix();
@@ -406,6 +362,52 @@ public class Cheaken extends AnimListener implements GLEventListener, MouseListe
         renderer.draw(fi, 550, 620);
         renderer.endRendering();
     }
+
+    @Override
+    public void init(GLAutoDrawable gld) {
+        GL gl = gld.getGL();
+        gl.glViewport(0, 0, 700, 700);
+//        gl.glClearColor(0.0f, 1.0f, 0.0f, 1.0f);    //This Will Clear The Background Color To Black
+        gl.glMatrixMode(GL.GL_PROJECTION);
+//        gl.glOrtho(-450, 450, -250, 250, -1.0, 1.0);
+        gl.glOrtho(0, 700, 0, 700, -1.0, 1.0);
+        
+        try {
+            music = new FileInputStream(new File("Music//chicken dance song.wav"));
+            audios = new AudioStream(music);
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
+        AudioPlayer.player.start(audios);
+
+        gl.glEnable(GL.GL_TEXTURE_2D);  // Enable Texture Mapping
+        gl.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA);
+
+        //number of textures, array to hold the indeces
+        gl.glGenTextures(textureName.length, textureIndex, 0);
+
+        for (int i = 0; i < textureName.length; i++) {
+            try {
+                texture = TextureReader.readTexture(assetsFolderName + "//" + textureName[i], true);
+                gl.glBindTexture(GL.GL_TEXTURE_2D, textureIndex[i]);
+
+//          mipmapsFromPNG(gl, new GLU(), texture[i]);
+                new GLU().gluBuild2DMipmaps(
+                        GL.GL_TEXTURE_2D,
+                        GL.GL_RGBA, // Internal Texel Format,
+                        texture.getWidth(), texture.getHeight(),
+                        GL.GL_RGBA, // External format from image,
+                        GL.GL_UNSIGNED_BYTE,
+                        texture.getPixels() // Imagedata
+                );
+            } catch (IOException e) {
+                System.out.println(e);
+                e.printStackTrace();
+            }
+        }
+    }
+  
+    
 
     @Override
     public void display(GLAutoDrawable gld) {
